0e0cbaee0052a5d4e4fa903a7607d77f787ad0f1
James Coonce <coonce.james@codebrains.io>
2019-12-27 11:55:55 -0500

    init commit

---
+++ webpack.server.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackConfigFactory = require('@nestjs/ng-universal')
+  .WebpackConfigFactory;
+
+const config = WebpackConfigFactory.create(webpack, {
+  // Nest server for SSR
+  server: './server/main.ts'
+});
+config.output = {
+  // Puts the output at the root of the dist folder
+  path: path.join(__dirname, 'dist'),
+  filename: '[name].js'
+};
+config.plugins = [
+  new webpack.ContextReplacementPlugin(
+    // fixes WARNING Critical dependency: the request of a dependency is an expression
+    /(.+)?angular(\\|\/)core(.+)?/,
+    path.join(__dirname, 'apps/client-ssr/src'), // location of your src
+    {} // a map of your routes
+  ),
+  new webpack.ContextReplacementPlugin(
+    // fixes WARNING Critical dependency: the request of a dependency is an expression
+    /(.+)?express(\\|\/)(.+)?/,
+    path.join(__dirname, 'apps/client-ssr/src'), {}
+  )
+];
+
+module.exports = config;
